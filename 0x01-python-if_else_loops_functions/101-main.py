043552ad13e846bfa0966827c7462792ec60ac96
phyatur <phyatur.official@gmail.com>
2023-06-06 22:32:42 +0000

    Task 15 (i. Refactoring to remove the function from working on negative n argument)

--- 0x01-python-if_else_loops_functions/101-main.py
+++ 0x01-python-if_else_loops_functions/101-main.py
@@ -6,3 +6,5 @@ print(remove_char_at("Chicago", 2))
 print(remove_char_at("C is fun!", 0))
 print(remove_char_at("School", 10))
 print(remove_char_at("Python", -2))
+print(remove_char_at("Chicago", -3))
+
