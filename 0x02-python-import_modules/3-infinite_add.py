84e71dd492dc9de1e4361be8fbdd64ccef72704e
phyatur <phyatur.official@gmail.com>
2023-06-08 20:32:55 +0000

    Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    length = len(sys.argv)
+    total = 0
+    for i in range(1, length):
+        total += int(sys.argv[i])
+    print('{}'.format(total))
