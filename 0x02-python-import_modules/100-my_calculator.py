fd754a5c5eff35b8a424d28ab1f6b1e00e9380f7
phyatur <phyatur.official@gmail.com>
2023-06-08 23:30:17 +0000

    Task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from  sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+
+    operator = argv[2]
+    first = int(argv[1])
+    second = int(argv[3])
+
+    if operator == '+':
+        result = add(first, second)
+
+    elif operator == '-':
+        result = sub(first, second)
+
+    elif operator == '*':
+        result = mul(first, second)
+
+    elif operator == '/':
+        result = div(first, second)
+
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+
+    print('{} {} {} = {}'.format(first, operator, second, result))
