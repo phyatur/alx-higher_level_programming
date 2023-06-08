f7da1fc73216985394a755265b0fbae28e597d8c
phyatur <phyatur.official@gmail.com>
2023-06-08 19:02:04 +0000

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+length = len(sys.argv)
+if length > 1:
+    print('{} argument:'.format(length-1))
+    for i in range(1, length):
+        print('{}: {}'.format(i, sys.argv[i]))
+elif length <= 1:
+    print('0 arguments.')
