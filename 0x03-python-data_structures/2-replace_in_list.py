5ee98c359ec449ca1a28f164ae36653412ea9753
phyatur <phyatur.official@gmail.com>
2023-06-12 23:21:08 +0000

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     length = len(my_list)
-    if idx < 0 or idx > length:
+    if idx < 0 or idx > length - 1:
         return my_list
     else:
         my_list[idx] = element
