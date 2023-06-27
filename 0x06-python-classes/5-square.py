1479be25c78256742e7056c20a46a11ead0ce8d5
phyatur <phyatur.official@gmail.com>
2023-06-27 23:39:47 +0000

    Task 5(i)

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -31,8 +31,7 @@ class Square:
             raise TypeError('size must be an integer')
         elif value < 0:
             raise ValueError('size must be >= 0')
-        else:
-            self.__size = value
+        self.__size = value
 
     def area(self):
         """
@@ -48,9 +47,10 @@ class Square:
         prints square with character #
         """
         if self.__size == 0:
-            print("")
+            print()
             return
         for i in range(self.__size):
-            print("#" * self.__size)
+            for j in range(self.__size):
+                print("#", end="")
+            print()
         return
-
