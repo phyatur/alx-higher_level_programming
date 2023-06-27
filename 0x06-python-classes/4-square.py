8c682d94c0b713ef8c72e7766c6f94e6c7243b74
phyatur <phyatur.official@gmail.com>
2023-06-27 22:18:56 +0000

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" defines a square based on size"""
+
+
+class Square:
+    """
+    class square with private instance attribute size
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: size of side of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        find size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Args:
+            value: value of size
+        """
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        area of square
+
+        Returns:
+            the area of the square
+        """
+
+        return self.__size ** 2
