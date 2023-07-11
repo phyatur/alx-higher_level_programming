55c27f430f57eb49761df9f8ed28fa438eedca06
phyatur <phyatur.official@gmail.com>
2023-07-11 22:37:20 +0000

    Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" load_from_json_file function """
+import json
+
+
+def load_from_json_file(filename):
+    """ a function that creates an Object from a JSON file """
+    with open(filename, encoding='utf-8') as f:
+        return json.load(f)
