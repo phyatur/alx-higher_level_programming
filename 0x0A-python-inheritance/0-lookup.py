6540ae31fb7b9f113a231eef399b6c1fda664218
phyatur <phyatur.official@gmail.com>
2023-07-11 00:21:29 +0000

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" module 0-lookup contains one function: lookup """
+
+
+def lookup(obj):
+    """ returns a list of available attributes and methods of an object """
+    return dir(obj)
