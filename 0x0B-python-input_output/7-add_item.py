cb42c59a115545f1ddb84b85106654727daea823
phyatur <phyatur.official@gmail.com>
2023-07-11 22:50:15 +0000

    Task 7(iii)

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ add_item function """
-from sys import argv
+import sys
+import json
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
