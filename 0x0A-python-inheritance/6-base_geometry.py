bf2bbeccfcd32535df35c3363937811e8244ef98
phyatur <phyatur.official@gmail.com>
2023-07-11 00:50:16 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" contains class BaseGeometry """
+
+
+class BaseGeometry:
+    """ defines class BaseGeometry """
+
+    def area(self):
+        """ raises an Exception """
+        raise Exception('area() is not implemented')
