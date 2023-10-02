4edbc602491ea99416a77997b2d23303d74cb821
phyatur <phyatur.official@gmail.com>
2023-10-02 20:46:41 +0000

    Task 2(ii)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -73,12 +73,12 @@ class Rectangle:
         """
         A method that returns the rectangle area
         """
-        return self.__height * self.__width
+        return self.height * self.width
 
     def perimeter(self):
         """
         A method that returns the rectangle perimeter
         """
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return 0;
-        return 2 * self.__height + 2 * self.__width
+        return 2 * self.height + 2 * self.width
