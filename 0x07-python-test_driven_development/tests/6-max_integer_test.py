5d542e5f64cb13fd8af55cb60f71713879f3888e
phyatur <phyatur.official@gmail.com>
2023-10-06 12:15:53 +0000

    Task 4

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    Test for '6-max_integer.py'
+    """
+
+    def test_normal(self):
+        """ Testing if it works """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([10, 3, 4, 2]), 10)
+
+    def test_negative(self):
+        """ Testing negatives """
+        self.assertEqual(max_integer([-1, -2, -2]), -1)
+        self.assertEqual(max_integer([-10, -3, -4]), -3)
+
+    def test_float(self):
+        """ Testing floats """
+        self.assertEqual(max_integer([1.5, 2.9, 3.66]), 3.66)
+        self.assertEqual(max_integer([10.90, 9.67 , 8.5]), 10.90)
+
+    def test_stringd(self):
+        """ Testing Strings """
+        self.assertEqual(max_integer(['python', 'zen', 'of']), 'zen')
+
+    def test_empty(self):
+        """ Testing empty list """
+        self.assertEqual(max_integer([]), None)
