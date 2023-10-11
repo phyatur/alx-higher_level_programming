5e91a505ee475b243733b35df4f95e083defc3e1
phyatur <phyatur.official@gmail.com>
2023-10-11 09:26:35 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A 5-base_geometry module that contains a class BaseGeometry """
+
+
+class BaseGeometry():
+    """ Defines class Geometry """
+
+    def area(self):
+        """ raises Exception """
+        raise Exception('area() is not implemented')
