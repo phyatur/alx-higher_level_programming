175eddaef6d93e9d748f7b95215490cd1dc4ac32
phyatur <phyatur.official@gmail.com>
2023-10-11 09:15:38 +0000

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A 4-inherits_from module that contains a function inherits_from """
+
+
+def inherits_from(obj, a_class):
+    """ returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
