8fb5ad7a8058d155a4de206a355f4a32fa63d924
phyatur <phyatur.official@gmail.com>
2024-03-13 15:47:32 +0000

    All Task

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- creates the table unique_id
+-- description of table data: id INT with the default value 1, must be unique & name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS unique_id (
+       id INT DEFAULT 1 UNIQUE,
+       name VARCHAR(256)
+);
